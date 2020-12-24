f888f4782ffc2d568be2f77bee4a65b242651da4
toloni <tiago.toloni@gmail.com>
2020-12-24 14:38:40 -0300

    first commit

---
+++ src/main/java/br/com/toloni/springbatchapp/SpringBatchAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.toloni.springbatchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/toloni/springbatchapp/SpringBatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.toloni.springbatchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
