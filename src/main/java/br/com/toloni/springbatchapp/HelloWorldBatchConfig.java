dd21d87fbb8ddc1ab44e3d52f2e79e6cecdc9728
toloni <tiago.toloni@gmail.com>
2020-12-24 17:26:31 -0300

    basic chunk

--- src/main/java/br/com/toloni/springbatchapp/HelloWorldBatchConfig.java
+++ src/main/java/br/com/toloni/springbatchapp/HelloWorldBatchConfig.java
@@ -6,10 +6,19 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 @Configuration
 public class HelloWorldBatchConfig {
@@ -21,16 +30,32 @@ public class HelloWorldBatchConfig {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job printHelloWorldJob() {
-        return jobBuilderFactory.get("printHelloWorldJob").start(printHelloWorldStep()).build();
+    public Job oddsOrEvensJob() {
+        return jobBuilderFactory.get("oddsOrEvensJob").start(oddsOrEvensStep()).build();
     }
 
-    private Step printHelloWorldStep() {
-        return stepBuilderFactory.get("printHelloWorldStep").tasklet(this::printHelloWorldTask).build();
+    private Step oddsOrEvensStep() {
+        return stepBuilderFactory.get("oddsOrEvensStep")
+                .<Integer, String>chunk(1)
+                .reader(countUntilTenReader())
+                .processor(oddsOrEvensProcessor())
+                .writer(resultWriter())
+                .build();
     }
 
-    private RepeatStatus printHelloWorldTask(StepContribution stepContribution, ChunkContext chunkContext) {
-        System.out.println("Hello World Spring Batch!");
-        return RepeatStatus.FINISHED;
+    private ItemWriter<String> resultWriter() {
+        return x -> x.forEach(System.out::println);
     }
+
+    private FunctionItemProcessor<Integer, String> oddsOrEvensProcessor() {
+        return new FunctionItemProcessor<Integer, String>
+                (x -> x % 2 == 0 ? "É PAR..: " + x : "É IMPAR: " + x);
+    }
+
+    private IteratorItemReader<Integer> countUntilTenReader() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return new IteratorItemReader<Integer>(list);
+    }
+
+
 }
